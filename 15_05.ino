42357439dab6fab90e77c59ba92287a4ff9aa1d3
Valentina Barrera A <valentina.barrera25@hotmail.com>
2021-05-15 12:13:18 -0500

    Ejemplo LED

--- 15_05.ino
+++ 15_05.ino
@@ -1,9 +1,13 @@
+#include <Led.h>
+Led ledRojo(13,false,0);
+Led ledAmarillo(12,false,0);
+Led Verde(11,false,0);
+
 void setup() {
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  ledRojo.encender();
 }
